76b80d7cec051b58706df360f1a24d92c44e25e9
10USDTony <88115006+10USDTony@users.noreply.github.com>
2021-09-29 08:48:05 -0500

    Update Staking.sol

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -1,4 +1,5 @@
-pragma solidity >=0.5.0 <0.6.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
